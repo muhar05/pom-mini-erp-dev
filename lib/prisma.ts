be975f0266c6125cce5bebf388f965a6648fa1ba
muhar05 <ferdiansyahmuh5@gmail.com>
2025-12-10 01:27:15 +0700

    fix: Remove datasourceUrl from PrismaClient initialization in production fix: Set engineType to binary in Prisma schema

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,10 +7,7 @@ export const prisma =
   new PrismaClient({
     log:
       process.env.NODE_ENV === "development" ? ["query", "error"] : ["error"],
-    ...(process.env.NODE_ENV === "production" && {
-      datasourceUrl: process.env.DATABASE_URL,
-    }),
-  } as any);
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
